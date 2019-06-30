722eef2fb2522300a65b4fbf0f4c7bc6a6cd7e81
Paquette, Adam <adampaquette@familiprix.com>
2019-06-30 18:45:00 -0400

    Fix analyse

--- src/Analyse.bat
+++ src/Analyse.bat
@@ -1,6 +1,7 @@
 @echo off
 dotnet sonarscanner begin /k:"Coverlet" /d:sonar.cs.opencover.reportsPaths=coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.verbose=true
+dotnet clean
 dotnet build Coverlet.sln
-coverlet Coverlet.MsTests\bin\Debug\netcoreapp2.2\Coverlet.Ms.Tests.dll --target "dotnet" --targetargs "test Coverlet.MsTests\Coverlet.Ms.Tests.csproj --no-build" --format opencover
+coverlet Coverlet.Ms.Tests\bin\Debug\netcoreapp2.2\Coverlet.Ms.Tests.dll --target "dotnet" --targetargs "test Coverlet.Ms.Tests\Coverlet.Ms.Tests.csproj --no-build" --format opencover
 dotnet sonarscanner end
 pause
