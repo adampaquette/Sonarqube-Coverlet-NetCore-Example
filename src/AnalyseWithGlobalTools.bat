f8aaf0f458bd36699329aba3bd4e3417ad27c281
Paquette, Adam <adampaquette@familiprix.com>
2019-06-30 22:14:52 -0400

    TESTS

--- src/AnalyseWithGlobalTools.bat
+++ src/AnalyseWithGlobalTools.bat
@@ -1,12 +1,16 @@
 dotnet build-server shutdown
+del coverage.opencover.xml
+del coverage.opencover1.xml
 pause
-dotnet sonarscanner begin /k:"Coverlet" /d:sonar.cs.opencover.reportsPaths=coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.verbose=true
+dotnet sonarscanner begin /k:"Coverlet" /d:sonar.cs.opencover.reportsPaths="coverage.opencover.xml" /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.verbose=true
 pause
 dotnet clean
 pause
 dotnet build
 pause
 coverlet Coverlet.Ms.Tests\bin\Debug\netcoreapp2.2\Coverlet.Ms.Tests.dll --target "dotnet" --targetargs "test Coverlet.Ms.Tests\Coverlet.Ms.Tests.csproj --no-build" --format opencover
+REM CANT MERGE BECAUSE IT CRASH...
+coverlet Coverlet.XUnit.Tests\bin\Debug\netcoreapp2.2\Coverlet.XUnit.Tests.dll --target "dotnet" --targetargs "test Coverlet.XUnit.Tests\Coverlet.XUnit.Tests.csproj --no-build" --format opencover --output coverage.opencover1.xml
 pause
 dotnet sonarscanner end
 pause
--- src/AnalyseWithNugetTools.bat
+++ src/AnalyseWithNugetTools.bat
@@ -1,3 +1,4 @@
+ECHO CURRENTLY NOT WORKING! FIX IT PLEASE
 dotnet build-server shutdown
 pause
 dotnet sonarscanner begin /k:"Coverlet" /d:sonar.cs.opencover.reportsPaths=coverage.opencover.xml /d:sonar.coverage.exclusions="**Test*.cs" /d:sonar.verbose=true
@@ -6,7 +7,8 @@ dotnet clean
 pause
 dotnet build
 pause
-dotnet test Coverlet.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[xunit*]*" /maxcpucount:1
+REM https://github.com/tonerdo/coverlet/issues/357
+dotnet test Coverlet.sln /p:CollectCoverage=true /p:CoverletOutputFormat=opencover /p:Exclude="[xunit*]*" /maxcpucount:1 /p:CoverletOutput="../coverage.opencover.xml" /p:MergeWith="../coverage.opencover.xml"
 pause
 dotnet sonarscanner end
 pause
